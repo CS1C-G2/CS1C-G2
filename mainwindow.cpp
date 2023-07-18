40061e1c4afa2886c7181fe1a563342611b288e4
Tim Arafeh <122069199+Speedyslaytaker@users.noreply.github.com>
2023-07-17 17:17:28 -0700

    Create mainwindow.cpp
    source file for gui

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include <QMenu>
+#include <QMenuBar>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    createMenus();
+}
+
+void MainWindow::createMenus()
+{
+    QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(tr("&About"), this, &MainWindow::about);
+}
+
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About 2D Graphics Modeler"),
+                       tr("This is the 2D Graphics Modeler application."));
+}
