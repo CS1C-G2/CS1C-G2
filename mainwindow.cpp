9e79b5c5a583b0874585c7754b1e091ed6ea4e34
Tim Arafeh <122069199+Speedyslaytaker@users.noreply.github.com>
2023-07-24 18:29:30 -0700

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,86 +1,50 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
-#include <QMenuBar>
 #include <QMenu>
+#include <QMenuBar>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    QFormLayout *layout = new QFormLayout;
+
+    shapeComboBox = new QComboBox;
+    createShapeButton = new QPushButton("Create Shape");
+
+    shapeComboBox->addItem("Square"); // add "Square" to the combo box
+    shapeComboBox->addItem("Circle"); // add "Circle" to the combo box
+    shapeComboBox->addItem("Triangle"); // add "Triangle" to the combo box
+
+    layout->addRow(shapeComboBox); // Add widgets to the layout.
+    layout->addRow(createShapeButton);
+
+    shapeComboBox->setFixedSize(200, 25); // Sets the size to 200px wide and 50px tall
+    createShapeButton->setFixedSize(200, 25); // Sets the size to 200px wide and 50px tall
+
+    QWidget *centralWidget = new QWidget;
+    centralWidget->setLayout(layout);
+    setCentralWidget(centralWidget); //Sets the size to 200px wide and 50px tall
+
+
+
     createMenus();
+
+    connect(createShapeButton, &QPushButton::clicked, this, &MainWindow::createShape);
 }
 
-void MainWindow::createMenus() {
-
-    // Create the menu bar
-    QMenuBar *menuBar = new QMenuBar(this);
-    setMenuBar(menuBar);
-
-    // Create the "File" menu and add it to the menu bar
-    QMenu *fileMenu = menuBar->addMenu("file");
-
-    // Create the "Contact Us" QAction and add it to the "File" menu
-    contactAction = new QAction("Contact Us", this);
-    helpAction = new QAction("help", this);
-    aboutAction = new QAction("About us", this);
-
-    fileMenu->addAction(contactAction);
-    fileMenu->addAction(helpAction);
-    fileMenu->addAction(aboutAction);
-
-    connect(contactAction, &QAction::triggered, this, &MainWindow::onContactUs);
-    connect(helpAction, &QAction::triggered, this, &MainWindow::onHelp);
-    connect(aboutAction, &QAction::triggered, this, &MainWindow::onAbout);
-
-} // createMenus
-
-void MainWindow::onContactUs() {
-    QDialog *contactDialog = new QDialog(this);
-    contactDialog->setWindowTitle("Contact Us");
-
-    QLabel *label = new QLabel("please email us at: someone@email.com", contactDialog);
-    label->setAlignment(Qt::AlignCenter);
-
-    QVBoxLayout *layout = new QVBoxLayout(contactDialog);
-    layout->addWidget(label);
-
-    contactDialog->exec();
-
-} //onContactUs
-
-void MainWindow::onHelp() {
-
-    QDialog *contactDialog = new QDialog(this);
-    contactDialog->setWindowTitle("Help");
-
-    QLabel *label = new QLabel("please email us at: someone@email.com", contactDialog);
-    label->setAlignment(Qt::AlignCenter);
-
-    QVBoxLayout *layout = new QVBoxLayout(contactDialog);
-    layout->addWidget(label);
-
-    contactDialog->exec();
-
-} // onHelp
-
-void MainWindow::onAbout() {
-    QDialog *contactDialog = new QDialog(this);
-    contactDialog->setWindowTitle("about");
-
-    QLabel *label = new QLabel("This is the 2D Graphics Modeler application", contactDialog);
-    label->setAlignment(Qt::AlignCenter);
-
-    QVBoxLayout *layout = new QVBoxLayout(contactDialog);
-    layout->addWidget(label);
-    contactDialog->exec();
-
-} // onAbout
-
-
-MainWindow::~MainWindow()
+void MainWindow::createMenus()
 {
-    delete ui;
+    QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(tr("&About"), this, &MainWindow::about);
 }
 
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About 2D Graphics Modeler"),
+                       tr("This is the 2D Graphics Modeler application."));
+}
 
+void MainWindow::createShape()
+{
+    QString selectedShape = shapeComboBox -> currentText();
+}
