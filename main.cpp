8ed22afb21279bd3b69fe82f53a785d7145268a1
Tim Arafeh <122069199+Speedyslaytaker@users.noreply.github.com>
2023-07-24 18:29:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <QApplication>
-#include "mainwindow.h"
-#include "loginwindow.h"
+#include "AdminWindow.h"
+#include "GuestWindow.h"
+#include "ShapeModel.h"
+#include "LoginWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
+    ShapeModel *model = new ShapeModel;
+
+    qDebug() << "Before LoginWindow creation";
     LoginWindow loginWindow;
+    qDebug() << "After LoginWindow creation";
+
+    QMainWindow *window;
+
     if (loginWindow.exec() == QDialog::Accepted) {
-        MainWindow mainWindow;
-        mainWindow.resize(800, 600);
-        mainWindow.show();
+        // Assume `isAdmin()` returns true if admin, false if guest
+        if (loginWindow.isAdmin()) {
+            window = new AdminWindow(model);
+        } else {
+            window = new GuestWindow(model);
+        }
+        window->resize(800, 600);
+        window->show();
         return app.exec();
     } else {
+        delete model;
         return 0;  // end the application if the login dialog is cancelled or login fails
     }
 }
